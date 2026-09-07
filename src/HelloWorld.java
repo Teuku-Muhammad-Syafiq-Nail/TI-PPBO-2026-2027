20bdc402ca549386adaf8646ce11eebf059ee50f
Teuku Muhammad Syafiq Nail <syafiqneel3@gmail.com>
2026-09-07 10:55:21 +0700

    Struktur Dasar Program Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Teuku Muhammad Syafiq Nail");
+        System.out.print("Nama: ");
+        System.out.print("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }

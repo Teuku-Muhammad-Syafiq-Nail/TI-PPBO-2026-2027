a16aaed5c6bda3da9bd52c9dfcda73eb9e25992d
Teuku Muhammad Syafiq Nail <syafiqneel3@gmail.com>
2026-09-07 11:39:32 +0700

    Deklarasi Variabel dan Tipe Data Primitif

---
+++ src/VariabelDemo.java
@@ -0,0 +1,15 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+        String nama = "teuku Muhammad Syafiq Nail";
+        int umur = 19;
+        double tinggi = 165.5;
+        char golDarah = 'o';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Golongan Darah: " + golDarah);
+        System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
